faa02d9184287ff5a4c63ff6428c06ddc4baf6b0
Mira V <this.email.abv.bg>
2025-06-10 14:36:43 +0300

    Fixing file location

--- Project/Project/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-require_once __DIR__ . '/../routes/Router.php';
-
-$router = new Router();
-require_once __DIR__ . '/../routes/routes.php';
-
-$router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
