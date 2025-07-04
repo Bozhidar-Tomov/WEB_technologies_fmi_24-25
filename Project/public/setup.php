67253ef48fdef3adfb2f229754db800bf1c937f7
Mira V <this.email.abv.bg>
2025-07-04 03:37:07 +0300

    Made the setup redirect automatically

--- Project/public/setup.php
+++ Project/public/setup.php
@@ -6,8 +6,23 @@ if (file_exists($script)) {
     ob_start();
     include $script;
     $output = ob_get_clean();
+    
+    // Show the output briefly
     echo '<pre>' . htmlspecialchars($output) . '</pre>';
-    echo '<p>RewriteBase updated! You can now delete this file for security.</p>';
+    echo '<p>RewriteBase updated! Redirecting to public directory...</p>';
+    
+    // Detect the correct redirect path
+    $scriptName = dirname($_SERVER['SCRIPT_NAME']);
+    $redirectPath = $scriptName !== '/' ? $scriptName : '';
+    
+    // Redirect to the public directory after a short delay
+    echo '<script>
+        setTimeout(function() {
+            window.location.href = "' . $redirectPath . '";
+        }, 1000);
+    </script>';
+    
+    echo '<p><a href="' . $redirectPath . '">Click here if you are not redirected automatically</a></p>';
 } else {
     echo 'update_htaccess.php not found.';
 } 
